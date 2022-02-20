811cda6eeb34e49dbfe0db0b779c20156932b643
HefnySco <mohammad.hefny@gmail.com>
2022-02-20 08:22:00 +0200

    f

--- examples/buzzer/test_buzzer.cpp
+++ examples/buzzer/test_buzzer.cpp
@@ -20,18 +20,20 @@ int main ()
         };
 
     cBuzzer.init(buzzer_pins);
-    cBuzzer.update();
-    cBuzzer.update();
+    cBuzzer.switchBuzzer(0,true,BARO_BUZZ);
+    
+    //cBuzzer.update();
     bool O = true;
     while (true)
     {
-        cBuzzer.on(0,O);
-        std::cout << "0:" << std::to_string(O) << std::endl;
-        cBuzzer.on(1,O);
-        std::cout << "1:" << std::to_string(O) << std::endl;
-        cBuzzer.on(2,O);
-        std::cout << "2:" << std::to_string(O) << std::endl;
-        O = !O;
+        cBuzzer.update();
+
+        // std::cout << "0:" << std::to_string(O) << std::endl;
+        // cBuzzer.on(1,O);
+        // std::cout << "1:" << std::to_string(O) << std::endl;
+        // cBuzzer.on(2,O);
+        // std::cout << "2:" << std::to_string(O) << std::endl;
+        // O = !O;
         usleep(100000); // 10Hz
     }
 }

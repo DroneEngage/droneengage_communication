18690eb46383096fa01b4066cc34a3b5e4f16cc9
HefnySco <mohammad.hefny@gmail.com>
2022-02-20 08:12:01 +0200

    f

--- examples/buzzer/test_buzzer.cpp
+++ examples/buzzer/test_buzzer.cpp
@@ -1,4 +1,5 @@
 
+#include <iostream>
 #include <stdint.h>
 #include <unistd.h>
 #include <vector>
@@ -25,8 +26,12 @@ int main ()
     while (true)
     {
         cBuzzer.on(0,O);
-        cBuzzer.on(1,!O);
-        O != O;
+        std::cout << "0:" << std::to_string(O) << std::endl;
+        cBuzzer.on(1,O);
+        std::cout << "1:" << std::to_string(O) << std::endl;
+        cBuzzer.on(2,O);
+        std::cout << "2:" << std::to_string(O) << std::endl;
+        O = !O;
         usleep(100000); // 10Hz
     }
 }

2f02753db9fac4ec39eddbaf01b5b1f6570317ed
HefnySco <mohammad.hefny@gmail.com>
2022-09-19 07:38:15 +0200

    fix: build

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 rm -rf ./build
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=DEBUG -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON  ../
+cmake -D CMAKE_BUILD_TYPE=RELEASE -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON  ../
 make 
 

2710eb68bc28f32a74cad009761509902c074c5d
Mohammad Hefny <mohammad.hefny@gmail.com>
2024-05-02 18:05:37 +0300

    build & config

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ rm -rf ./logs
 rm -rf ./build
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=DEBUG -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON  ../
+cmake -D CMAKE_BUILD_TYPE=RELEASE -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON  ../
 make 
 

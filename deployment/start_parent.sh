fe6c4cda8c7facd2479dedac19893c02c0f64f71
HefnySco <mohammad.hefny@gmail.com>
2026-06-21 18:15:56 +0300

    build:different configuration and build fix

---
+++ deployment/start_parent.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/mhefny/TDisk/public_versions/drone_engage/drone_engage_communication_pro/bin/de_comm -c /home/mhefny/TDisk/public_versions/drone_engage/drone_engage_communication_pro/deployment/de_comm.config.module.parent.json -b /home/mhefny/TDisk/public_versions/drone_engage/drone_engage_communication_pro/deployment/de_comm.config.module.parent.json.local
